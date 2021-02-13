02db9a5d94e0a417e1e8372b2e7abe49dd112d95
Aman Raj <amanrj2914@gmail.com>
2021-02-13 22:57:24 +0530

    nothing changed

--- app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
+++ app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContent
         setContentView(R.layout.activity_main)
     }
 }
